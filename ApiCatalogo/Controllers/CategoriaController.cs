d83d57b97353ca7c85f7ee2f4889e62fcac0f317
Otavio Augusto <134602837+0tavioPascoal@users.noreply.github.com>
2025-04-29 16:40:44 -0300

    Feat: Adicionando Controllers

---
+++ ApiCatalogo/Controllers/CategoriaController.cs
@@ -0,0 +1,78 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+    public class CategoriaController: ControllerBase {
+
+    private readonly AppDbContext _context;
+
+    public CategoriaController(AppDbContext context) {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Categoria>> GetCategorias() {
+        var categorias = _context.Categorias.ToList();
+
+        if(categorias == null) {
+            return NotFound();
+        }
+
+        return Ok(categorias);
+    }
+
+    [HttpGet("{id:int}", Name = "ObterCategoria")]
+    public ActionResult<Categoria> ObterPorId(int id) {
+        var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+
+        if(categoria == null) {
+            return NotFound("categoria não Encontrada");
+        }
+
+        return Ok(categoria);
+    }
+
+    [HttpPost]
+    public ActionResult<Categoria> Criar(Categoria categoria) {
+
+        _context.Categorias.Add(categoria);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("Obtercategoria", new { id = categoria.CategoriaId }, categoria);
+
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult<Categoria> Modificar(Categoria categoria, int id) {
+        if (id != categoria.CategoriaId) {
+            return BadRequest();
+        }
+
+        _context.Entry(categoria).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(categoria);
+
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult<Categoria> Deletar(int id) {
+        var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+
+        if (categoria == null) {
+            return NotFound("categoria não Encontrada");
+        }
+
+        _context.Categorias.Remove(categoria);
+        _context.SaveChanges();
+
+        return Ok();
+    }
+
+}
+
---
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,75 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProdutosController: ControllerBase {
+
+    private readonly AppDbContext _context;
+
+    public ProdutosController(AppDbContext context) {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Produto>> Get() {
+        var produtos = _context.Produtos.ToList();
+
+        if(produtos == null) {
+            return NotFound();
+        }
+
+        return Ok(produtos);
+    }
+
+    [HttpGet("{id:int}", Name = "ObterProduto")]
+    public ActionResult<Produto> Get(int id) {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+        if (produto == null) {
+            return NotFound("Produto não Encontrado!");
+        }
+        return Ok(produto);
+    }
+
+    [HttpPost]
+    public ActionResult<Produto> Post(Produto produto) {
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterProduto",
+            new {id = produto.ProdutoId }, produto);
+
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult<Produto> Put(Produto produto, int id) {
+        if (id != produto.ProdutoId) {
+            return BadRequest();
+        }
+
+        _context.Entry(produto).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(produto);
+    }
+
+
+    [HttpDelete("{id:int}")]
+    public ActionResult<Produto> Delete(int id ) {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+        if (produto == null) {
+            return NotFound("Produto Não Localizado!");
+        }
+
+        _context.Produtos.Remove(produto);
+        _context.SaveChanges();
+
+        return Ok();
+    }
+
+}
